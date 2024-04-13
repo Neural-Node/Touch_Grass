71a5d4d043bdf1cb215461010cd1ef2971328336
DHarrison <dharrison5@ewu.edu>
2024-04-13 11:37:53 -0700

    Auto stash before merge of "main" and "origin/main"

--- TouchGrass/TouchGrass/Timer.cs
+++ TouchGrass/TouchGrass/Timer.cs
@@ -1,7 +1,23 @@
 // Timer.cs
 namespace TouchGrass;
 
+namespace TouchGrass;
+
 public class Timer
 {
+    private readonly Stopwatch countUpTimer;
+
+    public Timer()
+    {
+        countUpTimer = new Stopwatch();
+        countUpTimer.Start();
+    }
+
+    public TimeSpan GetTime()
+    {
+        return countUpTimer.Elapsed;
+    }
+    
+    
     // Add properties and methods as needed
 }
