663201efad65ff563a4b20353c51caf68707042d
DHarrison <dharrison5@ewu.edu>
2024-04-13 12:15:37 -0700

    Started grass

--- TouchGrass/TouchGrass/Grass.cs
+++ TouchGrass/TouchGrass/Grass.cs
@@ -1,6 +1,23 @@
 // Grass.cs
+
+using System.Runtime.Versioning;
+using Microsoft.VisualBasic.CompilerServices;
+
 namespace TouchGrass;
 public class Grass
 {
-    // Add properties and methods as needed
+    private readonly int _xPosition;
+    private readonly int _yPosition;
+    
+    public Grass(int xSize, int ySize)
+    {
+        var rand = new Random();
+        
+        
+    }
+
+    public Grass()
+    {
+        this._xPosition = 
+    }
 }
