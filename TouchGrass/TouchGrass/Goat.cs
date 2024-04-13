55000440be31b7d74d3f41c7b35e691f70e79e68
Jesse Javana <jjavana@ewu.edu>
2024-04-13 11:39:42 -0700

    Goat Method
    Giving Goat something to do.

--- TouchGrass/TouchGrass/Goat.cs
+++ TouchGrass/TouchGrass/Goat.cs
@@ -3,4 +3,37 @@ namespace TouchGrass;
 public class Goat
 {
     // Add properties and methods as needed
+    private int movementSpeed;
+
+    //Constructor
+    public Goat (int speed)
+    {
+        this.movementSpeed = speed;
+    }
+
+    // Method to move the goat
+    public void Moved()
+    {
+        //Implement movement logic here
+    }
+
+    // Method for goat to eat grass
+    public void EatGrass()
+    {
+        // Implement eating grass logic here
+        Console.WriteLine("Goat eating grass");
+    }
+
+    // Method to have dialogue
+    public void Speak(string message)
+    {
+        // Implement dialogue logic here
+        Console.WriteLine($"Goat says: {message}")
+    }
+
+    // Getter and Setter fro movement speed
+    public int MovementSpeed
+    {
+        get { return this.movementSpeed;}
+        set { movementSpeed = value; }
 }
